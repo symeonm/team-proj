24bc95265dcf276bd2f9b668c9aacedc78d11480
symeonm <116294801+symeonm@users.noreply.github.com>
2023-02-02 23:41:04 +0000

    Deploying to gh-pages from @ symeonm/team-proj@8e766269b257d4bdd4476dd36e87479fb3f3db19 🚀

--- restaurant.2f70f03f.js
+++ restaurant.2f70f03f.js
@@ -1,2 +1,2 @@
 !function(){var o={openModalBtn:document.querySelector("[data-modal-room-open]"),closeModalBtn:document.querySelector("[data-modal-room-close]"),modal:document.querySelector("[data-modal-room]")};function e(){o.modal.classList.toggle("is-hidden")}o.openModalBtn.addEventListener("click",e),o.closeModalBtn.addEventListener("click",e)}();
-//# sourceMappingURL=index.30f19c68.js.map
+//# sourceMappingURL=restaurant.2f70f03f.js.map
--- restaurant.54797f08.js
+++ restaurant.54797f08.js
@@ -1,2 +1,2 @@
 !function(){var e={openModalBtn:document.querySelector("[data-modal-table-open]"),closeModalBtn:document.querySelector("[data-modal-table-close]"),modal:document.querySelector("[data-modal-table]")};function o(){e.modal.classList.toggle("is-hidden")}e.openModalBtn.addEventListener("click",o),e.closeModalBtn.addEventListener("click",o)}();
-//# sourceMappingURL=index.18d74197.js.map
+//# sourceMappingURL=restaurant.54797f08.js.map
