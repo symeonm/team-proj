47a49e94b6be0518577943e09fe9db78c8affb6a
symeonm <116294801+symeonm@users.noreply.github.com>
2023-02-03 16:27:38 +0000

    Deploying to gh-pages from @ symeonm/team-proj@3101c0eb8f68c127acd1f0eb2cb31586bcf422d9 🚀

---
+++ index.7c517149.js
@@ -0,0 +1,2 @@
+!function(){var o={openModalBtn:document.querySelector("[data-modal-room2-open]"),closeModalBtn:document.querySelector("[data-modal-room2-close]"),modal:document.querySelector("[data-modal-room2]")};function e(){o.modal.classList.toggle("is-hidden")}o.openModalBtn.addEventListener("click",e),o.closeModalBtn.addEventListener("click",e)}();
+//# sourceMappingURL=index.7c517149.js.map
