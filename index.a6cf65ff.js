0295ee0f6bc4c7142216a3490de7cd1e0cdfe3d3
symeonm <116294801+symeonm@users.noreply.github.com>
2023-02-03 12:42:52 +0000

    Deploying to gh-pages from @ symeonm/team-proj@20c8e356c82611581ae3f597ea7a4262450285d2 🚀

--- index.a6cf65ff.js
+++ index.a6cf65ff.js
@@ -1,2 +1,2 @@
 !function(){var e={openModalBtn:document.querySelector("[data-modal-pricel-open]"),closeModalBtn:document.querySelector("[data-modal-price-close]"),modal:document.querySelector("[data-modal-price]")};function o(){e.modal.classList.toggle("is-hidden")}e.openModalBtn.addEventListener("click",o),e.closeModalBtn.addEventListener("click",o)}();
-//# sourceMappingURL=room.72a92ae2.js.map
+//# sourceMappingURL=index.a6cf65ff.js.map
