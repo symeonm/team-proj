10426e929efcf060ce4f439ff2382b07af59d258
symeonm <m.simeon998@gmail.com>
2023-12-07 22:07:02 +0000

    Deploying to gh-pages from @ symeonm/team-proj@1dfe2b14f833a56f6807060b0a3ea58547c03ecb 🚀

--- index.a6cf65ff.js
+++
@@ -1,2 +0,0 @@
-!function(){var e={openModalBtn:document.querySelector("[data-modal-pricel-open]"),closeModalBtn:document.querySelector("[data-modal-price-close]"),modal:document.querySelector("[data-modal-price]")};function o(){e.modal.classList.toggle("is-hidden")}e.openModalBtn.addEventListener("click",o),e.closeModalBtn.addEventListener("click",o)}();
-//# sourceMappingURL=index.a6cf65ff.js.map
--- restaurant.2f70f03f.js
+++
@@ -1,2 +0,0 @@
-!function(){var o={openModalBtn:document.querySelector("[data-modal-room-open]"),closeModalBtn:document.querySelector("[data-modal-room-close]"),modal:document.querySelector("[data-modal-room]")};function e(){o.modal.classList.toggle("is-hidden")}o.openModalBtn.addEventListener("click",e),o.closeModalBtn.addEventListener("click",e)}();
-//# sourceMappingURL=restaurant.2f70f03f.js.map
